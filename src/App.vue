ac21352cd6479c5a73f545388d541de6b4ed4cb5
sukranyorulmazzzz <sukran_yrlmz@hotmail.com>
2024-05-31 10:52:28 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
+    <SplideSlide>
+      <img src="./assets/bg.jpg" alt="Sample 1" style="height: 100vh" />
+    </SplideSlide>
+    <SplideSlide>
+      <img src="./assets/bg.jpg" alt="Sample 2" style="height: 100vh" />
+    </SplideSlide>
+  </Splide>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Splide,
+    SplideSlide,
+  },
+};
 </script>
 
 <style>
+@import "@splidejs/vue-splide/css";
+
+@import "@splidejs/vue-splide/css/skyblue";
+@import "@splidejs/vue-splide/css/sea-green";
+
+@import "@splidejs/vue-splide/css/core";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueSplide from "@splidejs/vue-splide";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueSplide);
+app.mount("#app");
